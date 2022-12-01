85657c8608788a5487402d4e980515edf1d79980
Dika <mania.crussader2@gmail.com>
2022-12-01 14:25:35 +0700

    changing nameclass to Uppercase

--- app/src/main/java/com/example/capstonepesaing/Cart.kt
+++ app/src/main/java/com/example/capstonepesaing/Cart.kt
@@ -3,7 +3,7 @@ package com.example.capstonepesaing
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class cart : AppCompatActivity() {
+class Cart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
