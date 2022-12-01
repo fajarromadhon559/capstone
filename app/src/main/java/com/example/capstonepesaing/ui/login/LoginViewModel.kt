3e3e1c713d0df40aa0b92363c9ea352d4ad89cbf
Yuan <yuan.hardian2123@gmail.com>
2022-12-01 12:24:35 +0700

    Penambahan View model sementara

---
+++ app/src/main/java/com/example/capstonepesaing/ui/login/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.capstonepesaing.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel() {
+    fun loginUser(email: String, password: String){
+
+    }
+}
---
+++ app/src/main/java/com/example/capstonepesaing/ui/register/RegisterViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.capstonepesaing.ui.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel: ViewModel() {
+    fun registerUser(name: String, username: String, password: String){
+
+    }
+}
