e6b8308afac3522fce0681d5351ba39a1c21aaf2
Fajar Romadhon <fajar599@gmail.com>
2022-12-04 19:06:00 +0700

    Add intent

--- app/src/main/java/com/example/capstonepesaing/ui/login/LoginUser.kt
+++ app/src/main/java/com/example/capstonepesaing/ui/login/LoginUser.kt
@@ -1,12 +1,22 @@
 package com.example.capstonepesaing.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.capstonepesaing.R
+import com.example.capstonepesaing.databinding.ActivityLoginUserBinding
+import com.example.capstonepesaing.ui.home.MainActivity
 
 class LoginUser : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_user)
+        binding = ActivityLoginUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
     }
 }
--- app/src/main/java/com/example/capstonepesaing/ui/register/FinishRegister.kt
+++ app/src/main/java/com/example/capstonepesaing/ui/register/FinishRegister.kt
@@ -1,12 +1,25 @@
 package com.example.capstonepesaing.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.capstonepesaing.R
+import com.example.capstonepesaing.databinding.ActivityFinishRegisterBinding
+import com.example.capstonepesaing.ui.home.MainActivity
+import com.example.capstonepesaing.ui.login.LoginUser
 
 class FinishRegister : AppCompatActivity() {
+    private lateinit var binding : ActivityFinishRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_finish_register)
+        binding = ActivityFinishRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    //Jadi misal si user udah selesai regist gak perlu ke login lagi, langsung ke main
+        binding.btnFinish.setOnClickListener{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
     }
 }
