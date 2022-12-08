e8cffddb1c19010717d95cef8f7cd33bfd84078a
Dika <mania.crussader2@gmail.com>
2022-12-08 13:27:50 +0700

    Renew Response API

---
+++ app/src/main/java/com/example/capstonepesaing/data/response/UpResponse.kt
@@ -0,0 +1,9 @@
+package com.example.capstonepesaing.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class UpResponse(
+
+	@field:SerializedName("RESPONSE")
+	val rESPONSE: String? = null
+)
