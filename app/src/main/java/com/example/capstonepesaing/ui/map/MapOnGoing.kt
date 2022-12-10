cbe9aa34e6047e15707b076c42f5715a4c79c2fd
Fajar Romadhon <fajar599@gmail.com>
2022-12-10 13:20:17 +0700

    add maps on going

---
+++ app/src/main/java/com/example/capstonepesaing/ui/map/MapOnGoing.kt
@@ -0,0 +1,12 @@
+package com.example.capstonepesaing.ui.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstonepesaing.R
+
+class MapOnGoing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_on_going)
+    }
+}
