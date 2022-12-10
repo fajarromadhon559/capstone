182c1f329b47c8766035641b10b39c724102c3ae
Fajar Romadhon <fajar599@gmail.com>
2022-12-10 14:30:07 +0700

    add change password

---
+++ app/src/main/java/com/example/capstonepesaing/ui/login/ChangePassword.kt
@@ -0,0 +1,12 @@
+package com.example.capstonepesaing.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstonepesaing.R
+
+class ChangePassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
