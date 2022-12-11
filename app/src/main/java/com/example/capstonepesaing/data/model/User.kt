2ea9aac07767dd1cb54fe0c90a1468a8e55fa209
Dika <mania.crussader2@gmail.com>
2022-12-11 12:05:01 +0700

    Renew var saldo

--- app/src/main/java/com/example/capstonepesaing/data/model/User.kt
+++ app/src/main/java/com/example/capstonepesaing/data/model/User.kt
@@ -5,7 +5,6 @@ data class User (
     var nama : String?,
     var no_hp : String?,
     var email : String?,
-    var password : String?,
     var user_kategori : String?,
     var user_saldo : String?,
     var user_gambar : String?
